ba5182a533ae3dd4139682d72cd1be2cd9faa7b3
Laura Andstrom <laura.andstrom@helsinki.fi>
2021-04-26 18:08:57 +0300

    Vk 6 teht 4 tehty

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -3,46 +3,25 @@ package statistics;
 import statistics.matcher.*;
 
 public class Main {
+
     public static void main(String[] args) {
         String url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt";
 
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
-          
-        Matcher m = new And( new HasAtLeast(5, "goals"),
-                             new HasAtLeast(5, "assists"),
-                             new PlaysIn("PHI")
-        );
-        
-        for (Player player : stats.matches(m)) {
-            System.out.println(player);
-        }
-        
-        System.out.println(stats.matches(new All()).size());
-        
-        m = new And( new HasFewerThan(1, "goals"),
-                     new PlaysIn("NYR")
-        );
 
-        for (Player player : stats.matches(m)) {
-            System.out.println(player);
-        }
-
-        System.out.println(stats.matches(new All()).size());
-        
-        m = new And(
-                new HasAtLeast(50, "points"),
-                new Or(
-                        new PlaysIn("NYR"),
-                        new PlaysIn("NYI"),
-                        new PlaysIn("BOS")
-                )
-        );
+        QueryBuilder query = new QueryBuilder();
+        Matcher m = query.oneOf(
+                query.playsIn("PHI")
+                        .hasAtLeast(10, "assists")
+                        .hasFewerThan(5, "goals").build(),
+                query.playsIn("EDM")
+                        .hasAtLeast(40, "points").build()
+        ).build();
 
 
         for (Player player : stats.matches(m)) {
             System.out.println(player);
         }
-
-
     }
+
 }
---
+++ viikko6/QueryLanguage/src/main/java/statistics/matcher/QueryBuilder.java
@@ -0,0 +1,44 @@
+
+package statistics.matcher;
+
+
+public class QueryBuilder {
+    private Matcher matcher;
+   
+    public QueryBuilder(){   
+        this.matcher = new All();
+    }
+    
+    public void luoMatchers(Matcher uusi){
+        this.matcher = new And(matcher, uusi);        
+    }
+    
+    public Matcher build(){
+        return this.matcher;
+    }
+    
+    public QueryBuilder oneOf(Matcher...matchers){
+        this.matcher = new Or(matchers);
+        return this;
+    }
+    
+    public Matcher matches(Matcher m) {
+        this.matcher = m;
+        return matcher;
+    }
+    
+    public QueryBuilder playsIn(String team) {
+        luoMatchers(new PlaysIn(team));
+        return this;
+    } 
+    
+    public QueryBuilder hasAtLeast(int nr, String what) {
+        luoMatchers(new HasAtLeast(nr, what));
+        return this;
+    }
+    
+    public QueryBuilder hasFewerThan(int nr, String what) {
+        luoMatchers(new HasFewerThan(nr, what));
+        return this;
+    }
+}
